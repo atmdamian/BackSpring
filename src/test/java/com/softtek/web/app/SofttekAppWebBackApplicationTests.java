e24d5f92426eb5500b06cd3dd3cdc3602d126089
atmdamian <sudodamian@hotmail.com>
2020-12-10 14:14:43 -0600

    Pruebas unitario Controller ResponseEntity

--- src/test/java/com/softtek/web/app/SofttekAppWebBackApplicationTests.java
+++ src/test/java/com/softtek/web/app/SofttekAppWebBackApplicationTests.java
@@ -6,14 +6,21 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.softtek.web.app.controller.UsuarioController;
 import com.softtek.web.app.entity.usuario.Usuario;
+import com.softtek.web.app.response.ResponseMethod;
 import com.softtek.web.app.service.UsuarioService;
 
 @SpringBootTest
@@ -22,10 +29,14 @@ class SofttekAppWebBackApplicationTests {
 
 	@Autowired
 	UsuarioService usuarioService;
+	
+	@Autowired		
+	private UsuarioController usuarioController;
 
 	@Test
 	@DirtiesContext
 	public void save_test() {
+		LOGGER.info("TEST UNITARIO SAVE PERSIST");
 		Usuario usuario = new Usuario();
 		usuario.setNombre("TEST");
 		usuario.setUsuario("test");
@@ -36,18 +47,46 @@ class SofttekAppWebBackApplicationTests {
 	@Test
 	@DirtiesContext
 	public void finadAll_test() {
+		LOGGER.info("TEST UNITARIO FINDALL");
 		usuarioService.findAll();
 	}
 	
 	@Test
 	@DirtiesContext
 	public void finadAll_findByUsuario() {
+		LOGGER.info("TEST UNITARIO FINDBYUSUARIO");
 		List<Usuario> usuarios = usuarioService.findByUsuario("DAMIAN");
 		usuarios.stream().forEach(list->{
 			assertEquals("DAMIAN", list.getNombre());
 
 		});
 		
+	}
+	
+	@Test
+	public void agregarUsuario_test() {	
+		Gson g = new Gson(); 
+		Usuario usuario = new Usuario();
+		
+		usuario.setNombre("TEST");
+		usuario.setUsuario("test");
+		usuario.setPw("test");
+		
+		ResponseEntity<?>  httpResponse =usuarioController.agregarUsuario(usuario);
+		
+		LOGGER.info("STATUS CODE "+httpResponse.getStatusCode());	
+		LOGGER.info("BODY "+httpResponse.getBody());	
+		LOGGER.info("CODE "+httpResponse.getStatusCodeValue());
+		LOGGER.info("CLASS "+httpResponse.getClass());
+		LOGGER.info("HEADERS "+httpResponse.getHeaders());
+
+		
+		ResponseMethod response = g.fromJson((String) httpResponse.getBody(), ResponseMethod.class);
+
+		Assert.assertEquals(httpResponse.getStatusCodeValue(), 200);
+		Assert.assertEquals(httpResponse.getStatusCode(), HttpStatus.OK);
+		Assert.assertEquals("GUARDADO CORRECTAMENTE", response.getMensaje());
+		Assert.assertEquals("200", response.getEstatus());	
 	}
 	
 	
